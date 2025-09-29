8eaa012d190fee81c387f0166c48395ebb0550d1
Rozeritto RN <nowrozireza1@gmail.com>
2025-09-29 10:50:37 +0200

    update

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Aldor";
+            Console.WriteLine(name);
         }
     }
 }
